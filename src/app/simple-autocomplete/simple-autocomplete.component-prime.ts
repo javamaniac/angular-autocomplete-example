7a7467537c99eb8b8bcbbf35eb3bc2a9d8ee466a
Dominic Marcotte <dominic.marcotte2@usherbrooke.ca>
2024-02-14 10:28:23 -0500

    Clean

--- src/app/simple-autocomplete/simple-autocomplete.component-prime.ts
+++ src/app/simple-autocomplete/simple-autocomplete.component-prime.ts
@@ -24,102 +24,28 @@ interface AutoCompleteCompleteEvent {
   styleUrl: './simple-autocomplete.component-prime.css',
   imports: [
     FormsModule,
-    // MatFormFieldModule,
-    // MatInputModule,
-    // MatAutocompleteModule,
-    // ReactiveFormsModule,
-    // AsyncPipe,
-
     // PrimeNG
     AutoCompleteModule,
     RippleModule,
     StyleClassModule,
   ],
 })
-export class SimpleAutocompleteComponentPrime { //implements OnInit {
-
-  items: any[] | undefined;
-
-  selectedItem: any;
-
+export class SimpleAutocompleteComponentPrime {
+  myControl = new FormControl('');
   suggestions: any[] = [...countries];
 
+  items: any[] | undefined;
+  selectedItem: any;
+
   search(event: AutoCompleteCompleteEvent) {    
-      //this.suggestions = [...Array(200).keys()].map(item => event.query + '-' + item);
-      //console.log(this.suggestions, countries)
-      this.suggestions = [...countries].filter((suggestion) =>
+    this.suggestions = [...countries].filter((suggestion) =>
       suggestion.toLowerCase().includes(event.query.toLowerCase())      
     );
   }
-
-  // onFocus() {
-  //   // Simulez une entrée utilisateur pour ouvrir le panneau de suggestions.
-  //   // Vous pouvez passer une chaîne vide si vous souhaitez afficher toutes les suggestions,
-  //   // ou un caractère spécifique pour filtrer les suggestions initialement affichées.
-  //   //this.search({query: ''});
-  //   this.suggestions = [...countries]
-  // }
 
   onFocus(autoComplete: any) {
     // Solution temporaire pour forcer l'ouverture du panneau
     // Ceci est une approche non officielle et pourrait ne pas être fiable
     autoComplete.show()
-    // setTimeout(() => autoComplete.show(), 100);
   }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    
-    const res = [...countries].filter((suggestion) =>
-      suggestion.toLowerCase().includes(filterValue)      
-    );
-    // console.log('res', res)
-    return res
-  }
-
-  myControl = new FormControl('');
-  /*
-  
-  options: string[] = countries;
-  // filteredOptions?: Observable<string[]>;
-  // selectedCountry?: string;
-
-  ///
-  items: any[] | undefined;
-
-  selectedCountry: any;
-
-  suggestions: any[] = ['ww', 'qqq'];
-
-  search(event: AutoCompleteCompleteEvent) {
-    console.log(this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item))
-      this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
-
-  }
-  ///
-
-  ngOnInit() {
-    // this.filteredOptions = this.myControl.valueChanges.pipe(
-    //   startWith(''),
-    //   map((value) => this._filter(value || ''))
-    // );
-  }
-
-  // search(event: any) {
-  //   // console.log('search', event.query)
-  //   this.filteredOptions = of(this._filter(event.query));
-  //   // console.log('this.filteredOptions', this.filteredOptions)
-  // }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    
-    const res = this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue)      
-    );
-    console.log('res', res)
-    return res
-  }
-  */
 }
